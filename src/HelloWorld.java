798cd3e050bc4fa0d38e520c95e577e9a71ecc12
yunchoi <yunchoi@localhost>
2020-10-04 18:19:07 +0900

    add okc

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.print("Hello gogo World");
 		System.out.println("ARSENAL GOGO");
+		System.out.println("Okc GOGO");
 	}
 }
